182c49e5a89289e2413b57b91e5b311660fe812a
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2019-05-14 14:27:28 -0700

    updated front end GETs api

--- dcbr-api/operators/models.py
+++ dcbr-api/operators/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Operator(models.Model):
-      name = models.CharField(max_length=120)
+      name = models.CharField(max_length=40)
       address1 = models.CharField(max_length=200)
       timestamp = models.DateTimeField()
       numDogs = models.IntegerField()
--- dcbr-web/src/components/HelloWorld.vue
+++ dcbr-web/src/components/HelloWorld.vue
@@ -1,61 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-nightwatch" target="_blank" rel="noopener">e2e-nightwatch</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>Your name is {{ name }}</h1>
+        <input type="text" v-model="input.firstname" placeholder="First Name" />
+        <input type="text" v-model="input.lastname" placeholder="Last Name" />
+        <button v-on:click="sendData()">Send</button>
+        <br />
+        <br />
+        <textarea v-model= "this.response"/>
+    </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script>
+    import axios from "axios";
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+                name: "",
+                input: {
+                    firstname: "",
+                    lastname: ""
+                },
+                response: ""
+            }
+        },
+        mounted() {
+            axios({ method: "GET", "url": "http://127.0.0.1:8080/api/operators/1" }).then(result => {
+                this.name = result.data.name;
+            }, error => {
+                console.error(error);
+            });
+        },
+       methods: {
+            sendData() {
+                axios({ method: "POST", "url": "https://httpbin.org/post", "data": this.input, "headers": { "content-type": "application/json" } }).then(result => {
+                    this.response = result.data;
+                }, error => {
+                    console.error(error);
+                });
+            }
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    textarea {
+        width: 600px;
+        height: 200px;
+    }
 </style>
