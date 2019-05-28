3521c13ac6959c2a60ecdf0eba7724bd3f3ea6b7
Emiliano Suñé <emiliano.sune@gmail.com>
2019-05-28 14:03:12 -0700

    Add schema-spy. Update dcbr-api, dcbr-web Jenkinsfiles.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -4,7 +4,7 @@ export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export skip_git_overrides=""
+export skip_git_overrides="schema-spy-build.json"
 export GIT_URI="https://github.com/bcgov/agri-dcbr.git"
 export GIT_REF="master"
 
@@ -15,7 +15,7 @@ export components="dcbr-db dcbr-api dcbr-web"
 export builds=""
 
 # The images to be tagged after build
-export images="dcbr-db dcbr-api dcbr-web"
+export images="dcbr-db dcbr-api dcbr-web schema-spy"
 
 # The routes for the project
-export routes=""
+export routes="dcbr-db dcbr-api dcbr-web schema-spy"
