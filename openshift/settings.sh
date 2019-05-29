962b6197cf78286460c64bd6a4585f5a49fd89da
Emiliano Suñé <emiliano.sune@gmail.com>
2019-05-29 10:20:50 -0700

    Added backup container component.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -4,12 +4,12 @@ export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export skip_git_overrides="schema-spy-build.json"
+export skip_git_overrides="schema-spy-build.json backup-build.json"
 export GIT_URI="https://github.com/bcgov/agri-dcbr.git"
 export GIT_REF="master"
 
 # The project components
-export components="dcbr-db dcbr-api dcbr-web"
+export components="dcbr-db dcbr-api dcbr-web dcbr-backup"
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
 export builds=""
