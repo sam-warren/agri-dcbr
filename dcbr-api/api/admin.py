43f98be3836d7b1ae4e1da52e6430cca878e02e8
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2019-05-30 17:42:35 -0700

    models: operator, address, inspection, risk_factor_metadata, risk_factor_data, association membership

--- dcbr-api/api/admin.py
+++ dcbr-api/api/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from api.models import Operator, Address
+from api.models import (
+    Operator,
+    Address,
+    Inspection,
+    Risk_Factor_MetaData,
+    Risk_Factor_Data,
+    Association_Membership,
+)
 
-# from api.models import Category, Entry, Operator, Address
-
-# admin.site.register(Category)
-# admin.site.register(Entry)
 admin.site.register(Operator)
 admin.site.register(Address)
+admin.site.register(Inspection)
+admin.site.register(Risk_Factor_MetaData)
+admin.site.register(Risk_Factor_Data)
+admin.site.register(Association_Membership)
