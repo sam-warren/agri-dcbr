634e150b8618427a111a210b0540d9569c08f828
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-06-13 16:35:09 -0700

    Update build.js

--- .pipeline/lib/build.js
+++ .pipeline/lib/build.js
@@ -8,9 +8,21 @@ module.exports = (settings)=>{
   const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
   const phase='build'
   let objects = []
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../..'))
+
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dcbr-web/openshift/templates/dcbr-web-build.json`, {
+    'param':{
+      'NAME': `${phases[phase].name}${phases[phase].suffix}`,
+      'GIT_REPO_URL': oc.git.http_url,
+      'GIT_REF': oc.git.ref,
+      'SOURCE_CONTEXT_DIR': '.',
+      'DOCKER_FILE_PATH': 'docker/dcbr-web/Dockerfile',
+      'OUTPUT_IMAGE_TAG': phases[phase].tag
+    }
+  }));
 
   // The building of your cool app goes here ▼▼▼
+  
 
   oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
   oc.applyAndBuild(objects)
