7c59e066348b0eebe5702c36cafd7b741104f57f
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-20 16:04:27 -0700

    Update tslint.json, tsconfig.json

--- dcbr-web/src/main.ts
+++ dcbr-web/src/main.ts
@@ -10,29 +10,29 @@ import router from './router';
 
 Vue.config.productionTip = false;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   Vue.use(VueKeycloakJs, {
     config: {
-      url: 'https://sso-dev.pathfinder.gov.bc.ca/auth',
-      clientId: 'dcbr-web',
-      realm: 'uicn0kkh'
+      url: "https://sso-dev.pathfinder.gov.bc.ca/auth",
+      clientId: "dcbr-web",
+      realm: "uicn0kkh"
     },
     onReady: (keycloak: any) => {
       new Vue({
         router,
         render: h => h(App)
-      }).$mount('#app');
+      }).$mount("#app");
     }
   });
 } else {
-  Axios.get('/keycloak-config.json').then(response => {
+  Axios.get("/keycloak-config.json").then(response => {
     Vue.use(VueKeycloakJs, {
       config: response.data,
       onReady: (keycloak: any) => {
         new Vue({
           router,
           render: h => h(App)
-        }).$mount('#app');
+        }).$mount("#app");
       }
     });
   });
