d3d9db79722d3296e7b97c493dd56ce893d195fe
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-20 15:40:12 -0700

    Add default keycloak config for Vue app in development mode

--- dcbr-web/src/main.ts
+++ dcbr-web/src/main.ts
@@ -8,11 +8,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-Axios.get('/keycloak-config.json').then(response => {
-  Vue.config.productionTip = false;
+Vue.config.productionTip = false;
 
+if (process.env.NODE_ENV === 'development') {
   Vue.use(VueKeycloakJs, {
-    config: response.data,
+    config: {
+      url: 'https://sso-dev.pathfinder.gov.bc.ca/auth',
+      clientId: 'dcbr-web',
+      realm: 'uicn0kkh'
+    },
     onReady: (keycloak: any) => {
       new Vue({
         router,
@@ -20,4 +24,16 @@ Axios.get('/keycloak-config.json').then(response => {
       }).$mount('#app');
     }
   });
-});
+} else {
+  Axios.get('/keycloak-config.json').then(response => {
+    Vue.use(VueKeycloakJs, {
+      config: response.data,
+      onReady: (keycloak: any) => {
+        new Vue({
+          router,
+          render: h => h(App)
+        }).$mount('#app');
+      }
+    });
+  });
+}
