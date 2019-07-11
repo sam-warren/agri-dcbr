0e26df21cee9987ff1eace087862f05d0e33ad9c
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2019-07-11 12:36:56 -0700

    message change on Home.vue

--- dcbr-web/src/views/Home.vue
+++ dcbr-web/src/views/Home.vue
@@ -1,31 +1,8 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-card>
-        <v-flex ma-4>
-          <h2 class="text-md-center">You are on the AGRI / NRS page</h2>
-          <v-subheader>The four pillars are:</v-subheader>
-          <!-- <h4
-            class="text-md-center"
-          >Welcome to the official British Columbia Government Registry for commercial dog and cat breeders and sellers in British Columbia.</h4>-->
-          <!-- <br />This registry allows you to:
-          Submit registry information if you are a commercial breeder and/or seller in BC•View registry information of existing commercial breeders and/or sellers in BC, including the status of their registration•Obtain educational information related to commercial dog and cat breeding and/or selling in BCIn B.C., individuals and businesses breeding and/or selling dogs and cats in BC must provide registration information through this registry and confirm they are aware of and abide by the Commercial Dog and Cat Standards of Care Regulation. Why Register?This British Columbia Government Registry is part of a four-pillar policy framework for commercial dog and cat breeders and sellers in British Columbia.
-          <v-subheader>The four pillars are:</v-subheader>
-
-          <v-subheader>This regulatory framework was established to; 1) ensure a high level of animal care and well-being for dogs and cats bred and sold in BC.2) provide consumers with a means to verify a particular breeder or seller is adhering to the regulatory framework.</v-subheader>1. A repository where breeders/sellers will register their operation(s)
-          <br />2. A means for breeders/sellers to agree to follow a minimum standard of care.
-          <br />3. Communicate important documents (provincial minimum Standards of Care regulation) and educational documents with operators
-          <br />4.Provide the public with a place to search for registered breeders and their status.
-          <v-subheader>Who Must Register?</v-subheader>If you are a commercial breeder and/or seller of dogs and/or cats in British Columbia it is mandatory for you to register, provide accurate information, and follow the Standards of Care.
-          <v-subheader>Exceptions:</v-subheader>Persons or businesses that have a one-time accidental breeding whereby the resulting offspring are not sold, traded, nor leased. In this case, registration is not mandatory, nor is adhering to the Commercial Dog and Cat Breeders and Sellers Standards of Care. If persons or businesses plan to
-          sell, trade or lease the resulting offspring of a one-time accidental breeding, they must register and adhere to the Commercial Dog and Cat Breeders and Sellers Standards of Care.How to Register•Online (info...)•By Phone (info...)•By Mail or Fax (info..)-->
-        </v-flex>
-      </v-card>
-    </v-layout>
-    <v-layout>
       <v-flex>
         <v-btn large round block dark class="primary">
-          <button @click="$router.push('preamble')">Go to DCBR page</button>
+          <button @click="$router.push('preamble')">Click to start</button>
         </v-btn>
       </v-flex>
     </v-layout>
