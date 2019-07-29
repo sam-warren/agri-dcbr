c2a145fdd873c74b3b3d95c7e25f390809725ff0
Sam Warren <sam.warren@quartech.com>
2019-07-29 16:03:58 -0700

    Added :any type to state in mutations & getters

--- dcbr-web/src/store/registration/getters.ts
+++ dcbr-web/src/store/registration/getters.ts
@@ -5,47 +5,47 @@ import { RegistrationState } from "./types";
 
 export const getters: GetterTree<RegistrationState, RootState> = {
   //getters - profile
-  firstName(state): string {
+  firstName(state: any): string {
     const { profile } = state;
     return profile!.firstName || "";
   },
-  middleName(state): string {
+  middleName(state: any): string {
     const { profile } = state;
     return profile!.middleName || "";
   },
-  lastName(state): string {
+  lastName(state: any): string {
     const { profile } = state;
     return profile!.lastName || "";
   },
-  commType(state): string {
+  commType(state: any): string {
     const { profile } = state;
     return profile!.commType || "";
   },
-  phone(state): string {
+  phone(state: any): string {
     const { profile } = state;
     return profile!.phone || "";
   },
-  email(state): string {
+  email(state: any): string {
     const { profile } = state;
     return profile!.email || "";
   },
-  streetNumber(state): number {
+  streetNumber(state: any): number {
     const { profile } = state;
     return profile!.streetNumber || 0;
   },
-  aptNumber(state): number {
+  aptNumber(state: any): number {
     const { profile } = state;
     return profile!.aptNumber || 0;
   },
-  streetName(state): string {
+  streetName(state: any): string {
     const { profile } = state;
     return profile!.streetName || "";
   },
-  city(state): string {
+  city(state: any): string {
     const { profile } = state;
     return profile!.city || "";
   },
-  postalCode(state): string {
+  postalCode(state: any): string {
     const { profile } = state;
     return profile!.postalCode || "";
   }
--- dcbr-web/src/store/registration/mutations.ts
+++ dcbr-web/src/store/registration/mutations.ts
@@ -3,47 +3,47 @@ import { MutationTree } from "vuex";
 import { RegistrationState } from "../registration/types";
 
 export const mutations: MutationTree<RegistrationState> = {
-  firstName(state, payload: string) {
+  firstName(state: any, payload: string) {
     state.error = false;
     state.profile!.firstName = payload;
   },
-  middleName(state, payload: string) {
+  middleName(state: any, payload: string) {
     state.error = false;
     state.profile!.middleName = payload;
   },
-  lastName(state, payload: string) {
+  lastName(state: any, payload: string) {
     state.error = false;
     state.profile!.lastName = payload;
   },
-  commType(state, payload: string) {
+  commType(state: any, payload: string) {
     state.error = false;
     state.profile!.commType = payload;
   },
-  phone(state, payload: string) {
+  phone(state: any, payload: string) {
     state.error = false;
     state.profile!.phone = payload;
   },
-  email(state, payload: string) {
+  email(state: any, payload: string) {
     state.error = false;
     state.profile!.email = payload;
   },
-  streetNumber(state, payload: number) {
+  streetNumber(state: any, payload: number) {
     state.error = false;
     state.profile!.streetNumber = payload;
   },
-  aptNumber(state, payload: number) {
+  aptNumber(state: any, payload: number) {
     state.error = false;
     state.profile!.aptNumber = payload;
   },
-  streetName(state, payload: string) {
+  streetName(state: any, payload: string) {
     state.error = false;
     state.profile!.streetName = payload;
   },
-  city(state, payload: string) {
+  city(state: any, payload: string) {
     state.error = false;
     state.profile!.city = payload;
   },
-  postalCode(state, payload: string) {
+  postalCode(state: any, payload: string) {
     state.error = false;
     state.profile!.postalCode = payload;
   }
