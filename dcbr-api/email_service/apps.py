8aefae17803cf2301b64cba611d19085cc4e1e1d
Emiliano Suñé <emiliano.sune@gmail.com>
2019-08-01 09:51:58 -0700

    Working email service with scheduler and exposed API endpoint.

--- dcbr-api/email_service/apps.py
+++ dcbr-api/email_service/apps.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_migrate
 def start_reminder_email_task(sender, **kwargs):
     from .tasks import send_reminder_email
 
-    send_reminder_email(repeat=15, repeat_until=None)
+    send_reminder_email(repeat=15 * 60, repeat_until=None)
 
 
 class EmailServiceConfig(AppConfig):
