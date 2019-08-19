da08e82ac9da8869185b15067113ad05d57b9a67
Emiliano Suñé <emiliano.sune@gmail.com>
2019-08-19 14:19:39 -0700

    Add attachment to example registration email

--- dcbr-api/email_service/tasks.py
+++ dcbr-api/email_service/tasks.py
@@ -1,6 +1,10 @@
 import json
 
+import django.conf
+import requests
 from background_task import background
+from django.conf import settings
+from django.core.files.base import ContentFile
 from post_office import mail
 
 
@@ -18,11 +22,18 @@ def send_registration_email(email_addr):
 
     registration_number = "DCBR-123456"
 
+    response = requests.post(
+        settings.WEASYPRINT_REQUEST_URL + "certificate.pdf",
+        data="<h1>TEST CERTIFICATE</h1><p>A paragraph.</p>",
+        headers={"content-type": "text/html"},
+    )
+
     mail.send(
         email_addr,
         template="registration_email",  # Could be an EmailTemplate instance or name
         context={"user": json.dumps(user), "registration_number": registration_number},
         render_on_delivery=True,
+        attachments={"certificate.pdf": ContentFile(response.content)},
     )
 
     print("Registration confirmation sent to {}".format(email_addr))
