60151b0713846a0fdcdb42d97a81667f1b123ae7
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2019-08-28 14:51:48 -0700

    DCR-261: All models can be exported in csv format

--- dcbr-api/api/admin.py
+++ dcbr-api/api/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportMixin
+
+# from import_export.admin import ExportActionAdmin
+from api.resources import AddressResource
+
+from import_export.formats import base_formats
+
+
+class MyAdmin(ExportMixin):
+    # your normal stuff
+    def get_export_formats(self):
+        """
+            Returns available export formats.
+            """
+        formats = (base_formats.CSV,)
+        return [f for f in formats if f().can_export()]
+
+
 from api.models import (
     Registration_Number,
     Operator,
@@ -9,10 +28,40 @@ from api.models import (
     Association_Membership,
 )
 
-admin.site.register(Registration_Number)
-admin.site.register(Operator)
-admin.site.register(Address)
-admin.site.register(Inspection)
-admin.site.register(Operation_Risk_Factor)
-admin.site.register(Animal_Risk_Factor)
-admin.site.register(Association_Membership)
+
+class Registration_NumberAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class OperatorAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class AddressAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class InspectionAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class Operation_Risk_FactorAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class Animal_Risk_FactorAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+class Association_MembershipAdmin(MyAdmin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Registration_Number, Registration_NumberAdmin)
+admin.site.register(Operator, OperatorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Inspection, InspectionAdmin)
+admin.site.register(Operation_Risk_Factor, Operation_Risk_FactorAdmin)
+admin.site.register(Animal_Risk_Factor, Animal_Risk_FactorAdmin)
+admin.site.register(Association_Membership, Association_MembershipAdmin)
+
