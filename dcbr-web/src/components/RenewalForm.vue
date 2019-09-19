d949535ff0223741b3038d307994c080e1d2ab49
Sam Warren <sam.warren@quartech.com>
2019-09-19 12:19:37 -0700

    Add rules to renewal form

--- dcbr-web/src/components/RenewalForm.vue
+++ dcbr-web/src/components/RenewalForm.vue
@@ -1,73 +1,74 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-card-title primary-title>
-        <div>
-          <h2>Renewal</h2>
-        </div>
-      </v-card-title>
-      <v-flex xs12>
-        <v-card flat>
-          <v-form v-model="valid">
-            <v-container>
-              <!--Name Section  -->
-              <v-layout mx-2 mt-2>
-                <h4>Operator's Name</h4>
-              </v-layout>
-              <v-layout row wrap mx-2>
-                <v-flex xs12 md4>
-                  <v-text-field
-                    v-model="renewalFirstName"
-                    :rules="nameRules"
-                    label="First name"
-                    name="renewalFirstName"
-                    required
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-text-field
-                    v-model="renewalMiddleName"
-                    :rules="nameRules"
-                    label="Middle name (optional)"
-                    name="renewalMiddleName"
-                  ></v-text-field>
-                </v-flex>
+  <v-layout>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-card-title primary-title>
+          <div>
+            <h2>Renewal</h2>
+          </div>
+        </v-card-title>
+        <v-flex xs12>
+          <v-card flat>
+            <v-form v-model="valid">
+              <v-container>
+                <!--Name Section  -->
+                <v-layout mx-2 mt-2>
+                  <h4>Operator's Name</h4>
+                </v-layout>
+                <v-layout row wrap mx-2>
+                  <v-flex xs12 md4>
+                    <v-text-field
+                      v-model="renewalFirstName"
+                      :rules="nameRules"
+                      label="First name"
+                      name="renewalFirstName"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field
+                      v-model="renewalMiddleName"
+                      :rules="middleNameRules"
+                      label="Middle name (optional)"
+                      name="renewalMiddleName"
+                    ></v-text-field>
+                  </v-flex>
 
-                <v-flex xs12 md4>
-                  <v-text-field
-                    v-model="renewalLastName"
-                    :rules="nameRules"
-                    label="Last name"
-                    name="renewalLastName"
-                    required
-                  ></v-text-field>
-                </v-flex>
-              </v-layout>
+                  <v-flex xs12 md4>
+                    <v-text-field
+                      v-model="renewalLastName"
+                      :rules="nameRules"
+                      label="Last name"
+                      name="renewalLastName"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
 
-              <v-layout mx-2 mt-2>
-                <h4>Registration Number</h4>
-              </v-layout>
-              <v-layout row wrap mx-2>
-                <v-flex xs12 md4>
-                  <v-text-field
-                    :rules="nameRules"
-                    v-model="registrationNumber"
-                    label="Reg #"
-                    name="registrationNumber"
-                    required
-                  ></v-text-field>
-                </v-flex>
-
-                <v-flex xs12 md4>
-                  <DatePicker />
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-form>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+                <v-layout mx-2 mt-2>
+                  <h4>Registration Number</h4>
+                </v-layout>
+                <v-layout row wrap mx-2>
+                  <v-flex xs12 md4>
+                    <v-text-field
+                      :rules="registrationNumberRules"
+                      v-model="registrationNumber"
+                      label="Reg #"
+                      name="registrationNumber"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <DatePicker />
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-form>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-layout>
 </template>
 <script>
 
@@ -84,6 +85,13 @@ export default {
       v => !!v || "Name is required",
       v => v.length <= 50 || "Name must be less than 50 characters"
     ],
+    registrationNumberRules: [
+      v => !!v || "Registration number is required",
+      v => v.length <= 50 || "Registration number must be less than 50 characters"
+    ],
+    middleNameRules: [
+      v => v.length <= 50 || "Middle name must be less than 50 characters"
+    ]
   }),
   computed: {
     ...mapState({
