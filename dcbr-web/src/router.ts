ca6eedd7496e8e4677770bea9483ce69cece81d3
Emiliano Suñé <emiliano.sune@gmail.com>
2019-09-20 14:23:20 -0700

    Fix routing in vue app

--- dcbr-web/src/router.ts
+++ dcbr-web/src/router.ts
@@ -1,42 +1,36 @@
+import store from "@/store/store";
 import Vue from "vue";
 import Router from "vue-router";
 
-import Home from "./views/Home.vue";
-import OperationRegistration from "./views/OperationRegistration.vue";
-import Preamble from "./views/Preamble.vue";
-import Payment from "./views/Payment.vue";
-import Review from "./views/Review.vue";
-import Confirmation from "./views/Confirmation.vue";
-import Renew from "./views/Renew.vue";
-
-import store from "@/store/store"
-
 Vue.use(Router);
-
-// export default new Router({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes: [],
-// });
 
 const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  scrollBehavior () {
-    return {x: 0, y: 0}
+  scrollBehavior() {
+    return { x: 0, y: 0 };
   },
   routes: [
     {
+      path: "/",
+      name: "index",
+      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue"),
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
       path: "/home",
       name: "home",
-      component: Home
+      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue"),
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: "/preamble",
       name: "preamble",
-      component: () =>
-        // import(/* webpackChunkName: "secret" */ "./views/Secret.vue"),
-        import("./views/Preamble.vue"),
+      component: () => import(/* webpackChunkName: "preamble" */ "./views/Preamble.vue"),
       meta: {
         requiresAuth: false
       }
@@ -44,46 +38,26 @@ const router = new Router({
     {
       path: "/register",
       name: "register",
-      // component: () =>
-      //   import(
-      //     /* webpackChunkName: "register" */ "./views/OperatorRegistration.vue"
-      //   ),
-      //   import("./views/OperatorRegistration.vue"),
-      // meta: {
-      //   requiresAuth: false
-      // }
-      component: OperationRegistration
+      component: () => import(/* webpackChunkName: "registration" */ "./views/OperationRegistration.vue"),
+      meta: {
+        requiresAuth: false
+      }
     },
-    // {
-    //   path: "/payment",
-    //   name: "payment",
-    //   component: () =>
-    //     // import(/* webpackChunkName: "secret" */ "./views/Secret.vue"),
-    //     import("./views/Payment.vue"),
-    //   meta: {
-    //     requiresAuth: false
-    //   }
-    // },
     {
       path: "/review",
       name: "review",
-      component: () =>
-        // import(/* webpackChunkName: "secret" */ "./views/Secret.vue"),
-        import("./views/Review.vue"),
+      component: () => import(/* webpackChunkName: "review" */ "./views/Review.vue"),
       meta: {
         requiresAuth: false
-      },
-
+      }
     },
     {
       path: "/confirmation",
       name: "confirmation",
-      component: () =>
-        // import(/* webpackChunkName: "secret" */ "./views/Secret.vue"),
-        import("./views/Confirmation.vue"),
+      component: () => import(/* webpackChunkName: "confirmation" */ "./views/Confirmation.vue"),
       meta: {
         requiresAuth: false
-      },
+      }
     }
   ]
 });
@@ -92,37 +66,35 @@ router.beforeEach((to: any, from: any, next: any) => {
   if (to.path === "/") {
     if (store.getters["routeProtection/formType"] === "") {
       if (!(from.path === "/home")) {
-        next("/home")
+        next("/home");
       } else {
-        next()
+        next();
       }
     } else {
       next();
     }
-  }
-  else if (to.path === "/review") {
+  } else if (to.path === "/review") {
     if (!store.getters["routeProtection/registerFormOk"]) {
       if (!(from.path === "/home")) {
-        next("/home")
+        next("/home");
       } else {
-        next()
+        next();
       }
     } else {
       next();
     }
-  }
-  else if (to.path === "/confirmation") {
+  } else if (to.path === "/confirmation") {
     if (!store.getters["routeProtection/reviewFormOk"]) {
       if (!(from.path === "/home")) {
-        next("/home")
+        next("/home");
       } else {
-        next()
+        next();
       }
     } else {
-      next()
+      next();
     }
   } else {
-    next()
+    next();
   }
 });
 
