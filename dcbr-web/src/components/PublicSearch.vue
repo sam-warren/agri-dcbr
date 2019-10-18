7435582592f7804b20f1267a67cffdc08572c54b
Sam <samwarren4@gmail.com>
2019-10-18 11:32:01 -0700

    Prepopulated with dummy data

--- dcbr-web/src/components/PublicSearch.vue
+++ dcbr-web/src/components/PublicSearch.vue
@@ -10,7 +10,8 @@
             <v-text-field prepend-icon="search" label="Search for a breeder/seller" />
           </v-flex>
         </v-layout>
-          <v-data-table :headers="headers" :items="desserts" class="elevation-1">
+        <v-layout>
+          <v-data-table :headers="headers" :items="results" class="elevation-1">
             <template slot="headerCell" slot-scope="props">
               <v-tooltip bottom>
                 <template v-slot:activator="{ on }">
@@ -20,12 +21,9 @@
               </v-tooltip>
             </template>
             <template v-slot:items="props">
-              <td>{{ props.item.name }}</td>
-              <td class="text-xs-right">{{ props.item.calories }}</td>
-              <td class="text-xs-right">{{ props.item.fat }}</td>
-              <td class="text-xs-right">{{ props.item.carbs }}</td>
-              <td class="text-xs-right">{{ props.item.protein }}</td>
-              <td class="text-xs-right">{{ props.item.iron }}</td>
+              <td class="text-xs-left">{{ props.item.firstName }}</td>
+              <td class="text-xs-left">{{ props.item.lastName }}</td>
+              <td class="text-xs-left">{{ props.item.registrationNumber }}</td>
             </template>
           </v-data-table>
         </v-layout>
@@ -39,98 +37,29 @@ export default {
     return {
       headers: [
         {
-          text: "Dessert (100g serving)",
-          align: "left",
+          text: "First Name",
           sortable: false,
-          value: "name"
+          value: "firstName"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        { text: "Last Name", sortable: false, value: "lastName" },
+        { text: "Registration Number", sortable: false, value: "registrationNumber" },
       ],
-      desserts: [
+      results: [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
+          firstName: "Sam",
+          lastName: "Warren",
+          registrationNumber: "BC-DCBR-000001",
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
+          firstName: "Kevin",
+          lastName: "Sage",
+          registrationNumber: "BC-DCBR-000002",
         },
         {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
+          firstName: "Dean",
+          lastName: "Colpman",
+          registrationNumber: "BC-DCBR-000003",
         },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
       ]
     };
   }
